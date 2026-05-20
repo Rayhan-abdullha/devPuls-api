dd172f18ff4cd06b6c3e65014005788a455fd35e
rayhan_abdullha <captainrayhan215@gmail.com>
2026-05-21 00:34:46 +0600

    feat: added a home route

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Issue Tracker API");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/issues", issueRoutes);
 
